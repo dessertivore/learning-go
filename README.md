de83a209744ec09f7e6a39c0c5e8d02e56932f15
Sophie L <slandau@forestreet.com>
2024-12-23 11:52:42 +0000

    Add to Echo API

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# A basic API using Huma, with 2 endpoints
+# 2 basic APIs, using Huma and Echo, with 2 endpoints
 
-To start the server:
+To start the Huma server:
 ```sh
 go run api-huma/test_api.go
 ```
@@ -10,10 +10,19 @@ From another terminal:
 curl http://localhost:8888/restaurant
 ```
 
-### Endpoints:
+To start the Echo server:
+```sh
+go run api-echo/test_api.go
+```
+From another terminal:
+```sh
+curl http://localhost:8080/restaurant
+```
+
+### Endpoints - Huma:
 `/restaurant` - `GET`
 
-`/addition` - `POST` - e.g. `{"numsToAdd": "1,2,3,4,5"}` returns 
+`/addition` - `POST` - e.g. `curl -X POST http://127.0.0.1:8888/addition -H "Content-Type: application/json" -d '{"numsToAdd": "1,2,3,4,5"}'` returns 
 ```
 {
     "$schema": "http://127.0.0.1:8888/schemas/APIOutputBody.json",
@@ -28,3 +37,10 @@ go run requests/async_post.go
 This will call the API 10 times in quick succession, in an asynchronous fashion,
 time each total request as well as the full func. As it is async, even though each
 API call takes 5 seconds, the full func should also only take around 5 seconds.
+
+### Endpoints - Echo:
+`/` - `GET`
+`/restaurant` - `GET`
+`/restaurants` - `GET`
+
+`/addition` - `POST` - e.g. `curl -X POST http://127.0.0.1:8080/addition -H "Content-Type: application/json" -d '{"numsToAdd": "1,2,3,4,5"}'` returns `15`
--- api-echo/test_api.go
+++ api-echo/test_api.go
@@ -1,20 +1,64 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"testing_go_apis/data"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"golang.org/x/exp/rand"
 )
 
+func getRestaurants(c echo.Context) error {
+	return c.JSON(http.StatusOK, data.Restaurants)
+}
+type AdditionBody struct {
+	NumsToAdd string `json:"numsToAdd" xml:"numsToAdd" form:"numsToAdd" query:"numsToAdd"`
+}
+
+func addition(c echo.Context) error {
+	// Get nums to add from the request
+	nums := new(AdditionBody)
+	if err := c.Bind(nums); err != nil {
+		return err
+	}
+	// Split the string of numbers into an array of ints
+	numStrings := strings.Split(nums.NumsToAdd, ",")
+    numArray := make([]int, len(numStrings))
+    for i := range numArray {
+		numArray[i], _ = strconv.Atoi(numStrings[i])
+    }
+	
+	log.Printf("Nums to add: %v", numArray)
+
+	
+	var sum int
+	for _,num:= range numArray {
+		// Catch errors if invalid int provided
+		sum += num}
+		
+		// Sleep for 5 seconds to simulate a big query
+		time.Sleep(5 * time.Second)
+	
+	return c.String(http.StatusOK, strconv.Itoa(sum))
+	}
+
 func main() {
 	e := echo.New()
+	//  Basic Hello World
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	// Pick a random restaurant from the list
 	e.GET("/restaurant", func(c echo.Context) error {
 		return c.String(http.StatusOK, "You should go to "+data.Restaurants[rand.Intn(5)+1])
 	})
+	//  Return the dict of restaurants
+	e.GET("/restaurants", getRestaurants)
+	
+	e.POST("/addition", addition)
 	e.Logger.Fatal(e.Start(":8080"))
 }
